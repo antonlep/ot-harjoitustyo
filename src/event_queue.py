86fa32af27fb17628dfe3b0635186393571d05d5
antonlep <antonleppa@gmail.com>
2021-12-23 21:02:48 +0200

    Simplify event queue

--- src/event_queue.py
+++ src/event_queue.py
@@ -15,25 +15,33 @@ class EventQueue:
             if event.type == pygame.QUIT:
                 result.append("QUIT")
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    result.append("LEFT_DOWN")
-                if event.key == pygame.K_RIGHT:
-                    result.append("RIGHT_DOWN")
-                if event.key == pygame.K_UP:
-                    result.append("UP_DOWN")
-                if event.key == pygame.K_DOWN:
-                    result.append("DOWN_DOWN")
-                if event.key == pygame.K_RETURN:
-                    result.append("RETURN_DOWN")
-                if event.key == pygame.K_SPACE:
-                    result.append("SPACE_DOWN")
-                if event.key == pygame.K_n:
-                    result.append("N")
-                if event.key == pygame.K_SPACE:
-                    result.append("SPACE")
+                result = self._add_keydown_event(event, result)
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    result.append("LEFT_UP")
-                if event.key == pygame.K_RIGHT:
-                    result.append("RIGHT_UP")
+                result = self._add_keyup_event(event, result)
+        return result
+
+    def _add_keydown_event(self, event, result):
+        if event.key == pygame.K_LEFT:
+            result.append("LEFT_DOWN")
+        if event.key == pygame.K_RIGHT:
+            result.append("RIGHT_DOWN")
+        if event.key == pygame.K_UP:
+            result.append("UP_DOWN")
+        if event.key == pygame.K_DOWN:
+            result.append("DOWN_DOWN")
+        if event.key == pygame.K_RETURN:
+            result.append("RETURN_DOWN")
+        if event.key == pygame.K_SPACE:
+            result.append("SPACE_DOWN")
+        if event.key == pygame.K_n:
+            result.append("N")
+        if event.key == pygame.K_SPACE:
+            result.append("SPACE")
+        return result
+
+    def _add_keyup_event(self, event, result):
+        if event.key == pygame.K_LEFT:
+            result.append("LEFT_UP")
+        if event.key == pygame.K_RIGHT:
+            result.append("RIGHT_UP")
         return result
